54c0b4d59738d3e318fa5264fe5c21ecb9a631d6
Sanchit Padwekar <86397529+soulfaced@users.noreply.github.com>
2024-11-11 10:41:08 +0530

    changed url

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 
 // Set up CORS to allow requests from the frontend with credentials
 app.use(cors({
-    origin: 'https://chatbot-frontend-kuiqd10g4-soulfaceds-projects.vercel.app', // Update to match your frontend URL
+    origin: 'https://chatbot-frontend-soulfaced-soulfaceds-projects.vercel.app', // Update to match your frontend URL
     methods: ['GET', 'POST'],
     credentials: true // Allows cookies to be sent with requests
 }));
